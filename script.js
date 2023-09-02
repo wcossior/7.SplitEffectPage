842f9cd269202977afea58954511f18df64b4f2d
Wilder <w.cossio.r@gmail.com>
2023-09-02 19:27:26 -0400

    adding a state default when not hovering

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let ps5Side = document.querySelector('.ps5-side');
 let xboxSide = document.querySelector('.xbox-side');
+let container = document.querySelector('.container');
 
 ps5Side.addEventListener('mouseenter', () => {
     xboxSide.style.width = '25%';
@@ -9,4 +10,9 @@ ps5Side.addEventListener('mouseenter', () => {
 xboxSide.addEventListener('mouseenter', () => {
     ps5Side.style.width = '25%';
     xboxSide.style.width = '75%';
+});
+
+container.addEventListener('mouseleave', () => {
+    ps5Side.style.width = '50%';
+    xboxSide.style.width = '50%';
 });
